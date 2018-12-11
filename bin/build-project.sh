fe57fc95a0a4a0be3a0ea907400e0cb4b72d5d76
brahimRizq <brahim@hireme.io>
2018-12-11 14:45:29 +0100

    add PSR-2 coding standard

--- bin/build-project.sh
+++ bin/build-project.sh
@@ -7,3 +7,4 @@ docker-compose run $1 composer install
 #docker-compose run $1 composer development-enable
 
 # composer config minimum-stability dev
+# composer require --dev escapestudios/symfony2-coding-standard:3.x-dev
